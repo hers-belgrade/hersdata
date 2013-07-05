f0540ca3b1f9a91bbb10c18ed7539beac02cc4dc
Andrija Petrovic <andrija.hers@gmail.com>
2013-07-05 14:54:09 +0200

    little changes

--- datahive.js
+++ datahive.js
@@ -1,5 +1,8 @@
 function DataHive(){
   this.master = new (require('./datamaster').Collection)();
+  this.master.onNewTransaction.attach(function(path,txnalias,txnoperations){
+    console.log(path,txnalias,txnoperations);
+  });
 }
 DataHive.prototype.attach = function attach(objorname,config){
   return this.master.attach(objorname,config);
