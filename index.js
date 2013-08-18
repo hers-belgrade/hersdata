8cd543015992b6d76a7b8af71b0b0c000c4dcd7e
Andrija Petrovic <andrija.hers@gmail.com>
2013-08-18 11:43:51 +0200

    Moving towards exporting raw js files

--- index.js
+++ index.js
@@ -2,38 +2,17 @@ var util = require('util');
 var vm = require('vm');
 var fs = require('fs');
 
-/*NEED REWORK ....*/
-function codeAppendedFromFiles(paths){
-  var code = '';
-  for(var i in paths){
-    code += fs.readFileSync(paths[i]);
-    code += "\n";
+function codeOf(filename){
+  try{
+    return fs.readFileSync(filename);
   }
-  return code;
-}
-
-function glueCode(paths){
-  var code = codeAppendedFromFiles([__dirname+'/hooks.js',__dirname+'/datacopy.js',__dirname+'/datamaster.js']);
-  if(util.isArray(paths)){
-    code += codeAppendedFromFiles(paths);
+  catch(e){
+    return '';
   }
-  if(typeof paths === 'string'){
-    code += fs.readFileSync(paths);
-  }
-  return code;
-}
-
-function execCode(code){
-  var ret = function() {
-    vm.runInThisContext(code, 'glueCode');
-  }.bind(this);
-  return ret;
 };
 
-
 module.exports = {
-  glueCode : glueCode,
-  execCode : execCode,
+  codeOf : codeOf,
   Hive : require('./datahive'),
 	helpers: require('./helpers')
 };
