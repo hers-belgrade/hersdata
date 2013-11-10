26b2f0bd53a783261f8af26360b2ab34fa65f33f
Luka111 <lukapetrovicsi@gmail.com>
2013-11-10 14:17:33 +0100

    It was about time to move all that ConsumerIdentity stuff away

--- WebCollectionReplica.js
+++ WebCollectionReplica.js
@@ -1,46 +1,10 @@
 var ChildProcessCollectionReplica = require('./ChildProcessCollectionReplica');
-var ConsumerLobby = require('./consumers');
 
 function WebCollectionReplica(realmname){
   ChildProcessCollectionReplica.call(this,realmname);
-  this.lobby = new ConsumerLobby(this); 
   this.attach(__dirname+'/sessionuserfunctionality',{realmname:realmname});
 };
 WebCollectionReplica.prototype = new ChildProcessCollectionReplica();
 WebCollectionReplica.prototype.constructor = WebCollectionReplica;
-WebCollectionReplica.prototype.dumpQueue = function(params){
-  var cb = params.cb;
-  if(typeof cb !=='function'){return;}
-  var ic = this.lobby.identityAndConsumerFor(params);
-  if(ic){
-    ic[1].dumpqueue(cb);
-  }else{
-    cb();
-  }
-};
-WebCollectionReplica.prototype.invoke1 = function(params,cb){
-  var ic = this.lobby.identityAndConsumerFor(params);
-  if(!ic){
-    return cb('NO_USER');
-  }
-  return ChildProcessCollectionReplica.prototype.invoke.call(this,params.path,params.params,ic[0].name,'player,admin',cb);
-};
-WebCollectionReplica.prototype.setUser1 = function(username,realmname,roles,cb){
-  var ic = this.lobby.identityAndConsumerFor({name:username});
-  if(ic){
-    cb(ic[0]);
-  }else{
-    cb();
-  }
-};
-WebCollectionReplica.prototype.findUser = function(username,realmname,cb){
-  var ic = this.lobby.identityAndConsumerFor({name:username});
-  if(ic){
-    cb(ic[0]);
-  }
-};
-WebCollectionReplica.prototype.removeUser = function(data){
-  console.log('removeUser',data);
-};
 
 module.exports = WebCollectionReplica;
