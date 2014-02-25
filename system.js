8622092c12fe5030fa07162d5b9029656e9b011c
Andrija Petrovic <andrija.hers@gmail.com>
2014-02-25 18:48:20 +0100

    better system stats

--- system.js
+++ system.js
@@ -7,43 +7,32 @@ var errors = {
 
 function doMetrics(data){
   var m = Timeout.metrics();
-  console.log('timeout metrics',m);
+  //console.log('timeout metrics',m);
   var nm = ReplicatorCommunication.metrics();
-  console.log('network metrics',nm);
+  //console.log('network metrics',nm);
   var mu = data.element(['memoryusage']).value();
   var mmu = Math.floor(process.memoryUsage().rss/1024/1024);
   var actions = [];
-  if(m.utilization){
-    actions.push(['set',['CPU'],[m.utilization,undefined,'dcp']]);
-  }
-  if(m.delay){
-    actions.push(['set',['exec_delay'],[m.delay,undefined,'dcp']]);
-  }
-  if(m.queue && m.queue.current){
-    actions.push(['set',['exec_queue'],[m.queue.current,undefined,'dcp']]);
-  }
-  if(nm.traffic){
-    if(nm.traffic.rx){
-      actions.push(['set',['network_in'],[nm.traffic.rx,undefined,'dcp']]);
-    }
-    if(nm.traffic.tx){
-      actions.push(['set',['network_out'],[nm.traffic.tx,undefined,'dcp']]);
-    }
-  };
-  console.log('memory usage',mmu);
+  actions.push(['set',['CPU'],[m.utilization||0,undefined,'dcp']]);
+  actions.push(['set',['exec_delay'],[((~~(m.delay*100)/100)||0)+'ms',undefined,'dcp']]);
+  actions.push(['set',['exec_queue'],[m.queue && m.queue.current ? m.queue.current : 0,undefined,'dcp']]);
+  actions.push(['set',['network_in'],[(nm.traffic&&nm.traffic.rx ? nm.traffic.rx : 0)+'bytes',undefined,'dcp']]);
+  actions.push(['set',['network_out'],[(nm.traffic&&nm.traffic.tx ? nm.traffic.tx : 0)+'bytes',undefined,'dcp']]);
+  //console.log('memory usage',mmu);
   if(mu!==mmu){
-    actions.push(['set',['memoryusage'],[mmu,undefined,'system']]);
+    actions.push(['set',['memoryusage'],[mmu+'MB',undefined,'system']]);
   }
   var ma = data.element(['memoryavailable']).value();
   var mma = Math.floor(os.freemem()/1024/1024);
   if(ma!==mma){
-    actions.push(['set',['memoryavailable'],[mma,undefined,'system']]);
+    actions.push(['set',['memoryavailable'],[mma+'MB',undefined,'system']]);
   }
-  if(actions.length){
-    //console.log('commiting memoryusagechanged',now);
-    console.log('dcp',data.instanceCounts());
-    data.commit('system_metrics_changed',actions);
-  }
+  var ic = data.instanceCounts();
+  //console.log('commiting memoryusagechanged',now);
+  //console.log('dcp',data.instanceCounts());
+  actions.push(['set',['dcp_branches'],[ic && ic.collections ? ic.collections : 0,undefined,'dcp']]);
+  actions.push(['set',['dcp_leaves'],[ic && ic.scalars ? ic.scalars : 0,undefined,'dcp']]);
+  data.commit('system_metrics_changed',actions);
   Timeout.set(doMetrics,10000,data);
 };
 
