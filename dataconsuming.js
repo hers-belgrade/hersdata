8f610c38340abdfcc6889fcd92783e8c0ddedb7b
Andrija Petrovic <andrija.hers@gmail.com>
2014-02-26 14:42:17 +0100

    less debugging

--- dataconsuming.js
+++ dataconsuming.js
@@ -488,7 +488,7 @@ ConsumingCollection.prototype.upgradeUserToConsumer = function(u){
   }
   u.sessions = {};
   u.follow = function(path,cb){
-    console.log('follow',path);
+    //console.log('follow',path);
     if(path.path){
       path = path.path;
     }
