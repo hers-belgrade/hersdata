f40ef3d24eb64619702d5875d5d02798837483ee
Andrija Petrovic <andrija.hers@gmail.com>
2014-03-13 17:15:37 +0100

    fix

--- datamaster.js
+++ datamaster.js
@@ -972,14 +972,14 @@ Collection.prototype.doUserDescribe = function(username,realmname,cb){
   }
 };
 
-Collection.prototype.doUserFollow = function(username,realmname,path,cb){
+Collection.prototype.doUserFollow = function(username,realmname){
   //console.log('doUserFollow',username,realmname,Array.prototype.slice.call(arguments,2));
   var u = UserBase.findUser(username,realmname);
   if(u){
     if(!u.follow){
       return;
     }
-    u.follow(Array.prototype.slice.call(arguments,2,-1),cb);
+    u.follow(Array.prototype.slice.call(arguments,2,-1),arguments[arguments.length-1]);
   }
 };
 
