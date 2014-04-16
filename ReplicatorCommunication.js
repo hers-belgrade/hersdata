bf658779e4bfc143a38083fa8c4329a724d8dfa8
Andrija Petrovic <andrija.hers@gmail.com>
2014-04-16 10:55:47 +0200

    more debugging

--- ReplicatorCommunication.js
+++ ReplicatorCommunication.js
@@ -17,7 +17,7 @@ function userStatus(replicatorcommunication){
 function userSayer(replicatorcommunication){
   var rc = replicatorcommunication;
   return function(item){
-    console.log('userSayer',item,'on',this.fullname);
+    console.log('userSayer',item,'on',this.fullname,this.keys);
     rc.send('usersay',this._replicationid,item);
   }
 }
