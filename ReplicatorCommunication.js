69be536e82f1b5be6a97adf5320630369fe51c7d
Andrija Petrovic <andrija.hers@gmail.com>
2014-04-24 14:44:59 +0200

    Fix: call instead of apply in destruction handler of DataFollower

--- ReplicatorCommunication.js
+++ ReplicatorCommunication.js
@@ -17,7 +17,10 @@ function userStatus(replicatorcommunication){
 function userSayer(replicatorcommunication){
   var rc = replicatorcommunication;
   return function(item){
-    //console.log('userSayer',item,'on',this.fullname);
+    /*
+    console.trace();
+    console.log('userSayer',item,'on',this.fullname);
+    */
     rc.send('usersay',this._replicationid,item);
   }
 }
